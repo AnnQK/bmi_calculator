2e0ef96b6ab440e50217933b29456e487fcb3195
Ann <annhuddy13@gmail.com>
2021-12-08 22:03:49 +0300

    upgraded code with function

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,7 +11,22 @@ const sectionResultComment = document.querySelector(
   '.mainSection__result__comment'
 );
 let commentForResult;
+// functions
 
+const calculatingFunction = function () {
+  const userWeight = Number(document.getElementById('userWeight').value);
+  let userHeight = Number(document.getElementById('userHeight').value);
+  // cm to m
+  userHeight = userHeight / 100;
+  let userBmi = (userWeight / (userHeight * userHeight)).toFixed(2);
+  sectionPreview.classList.add('hidden');
+  sectionResult.classList.remove('hidden');
+  sectionResultNumber.textContent = userBmi;
+  commentBmi(userBmi);
+  sectionResultComment.textContent = commentForResult;
+};
+
+// comment depending on bmi
 const commentBmi = function (userBmi) {
   if (userBmi <= 18.5) {
     commentForResult = 'Недостаток массы тела :(';
@@ -22,15 +37,7 @@ const commentBmi = function (userBmi) {
   }
 };
 
-// to calculate thr bmi
-document.querySelector('.btn-calculate').addEventListener('click', function () {
-  const userWeight = Number(document.getElementById('userWeight').value);
-  let userHeight = Number(document.getElementById('userHeight').value);
-  userHeight = userHeight / 100;
-  let userBmi = (userWeight / (userHeight * userHeight)).toFixed(2);
-  sectionPreview.classList.add('hidden');
-  sectionResult.classList.remove('hidden');
-  sectionResultNumber.textContent = userBmi;
-  commentBmi(userBmi);
-  sectionResultComment.textContent = commentForResult;
-});
+// to calculate bmi
+document
+  .querySelector('.btn-calculate')
+  .addEventListener('click', calculatingFunction);
